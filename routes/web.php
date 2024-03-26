bbe14a24727aca070c635d94069d4299befb9e59
shakirr45 <shakirr45@gmail.com>
2024-03-26 20:45:01 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('users', UserController::class);
     Route::resource('products', ProductController::class);
 });
+
+
+
+// end 
